c3558c62e5f2dc333ca7ca157dce7136a9b54d4f
crealu <cameron.miller294@gmail.com>
2023-02-16 23:54:30 -0500

    Update tartle 10
    - Added a DayKanji class

--- tartle10/tart.py
+++ tartle10/tart.py
@@ -49,15 +49,49 @@ class EyeKanji:
 		self.turt.pendown()
 		self.turt.forward(30)
 
+class DayKanji:
+	def __init__(self, x0, y0):
+		self.turt = turtle.Turtle()
+		self.x = x0
+		self.y = y0
+
+	def setup(self):
+		self.turt.hideturtle()
+		self.turt.penup()
+		self.turt.width(5)
+		self.turt.goto(self.x, self.y)
+
+	def draw(self):
+		self.turt.pendown()
+		self.turt.setheading(270)
+		self.turt.forward(45)
+		self.turt.penup()
+		self.turt.goto(self.x, self.y)
+		self.turt.setheading(0)
+		self.turt.pendown()
+		self.turt.forward(30)
+		self.turt.setheading(270)
+		self.turt.forward(45)
+		self.turt.penup()
+		self.turt.goto(self.x, self.y - 21)
+		self.turt.setheading(0)
+		self.turt.pendown()
+		self.turt.forward(30)
+		self.turt.penup()
+		self.turt.goto(self.x, self.y - 42)
+		self.turt.setheading(0)
+		self.turt.pendown()
+		self.turt.forward(30)
+
 def run():
 	eye = EyeKanji(0, 0)
 	eye.setup()
 	eye.draw()
+
+	day = DayKanji(100, 0)
+	day.setup()
+	day.draw()
+
 	img = ImageGrab.grab(bbox=bounds)
 	img.save(this_dir + '/tartle10/tart.png', quality=95)
-	readme = open('README.md', 'a')
-	readme.write('\n')
-	readme.write('![alt text](./tartle10/tart.png)')
-	readme.write('tartle 10')
-	readme.write('&nbsp;')
 
