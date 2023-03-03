b1685cf6e03cc26390aaca084ae466526f245127
crealu <cameron.miller294@gmail.com>
2023-03-02 23:16:22 -0500

    Update tartle11

--- tartle11/tart.py
+++ tartle11/tart.py
@@ -10,41 +10,44 @@ this_dir = os.getcwd()
 bounds = (366, 140, 1060, 800)
 
 class Arc:
-	def __init__(self, x0, y0):
+	def __init__(self, angle):
 		self.turt = turtle.Turtle()
-		self.x = x0
-		self.y = y0
+		self.x = angle
+		self.y = 10
 		self.radius = 20
-		self.new_heading = 90 + x0
-		self.amp = 10
-		self.freq = 10
-		self.phase = 8
+		self.new_heading = 90 + angle
+		self.amp = 40
+		self.freq = math.pi/10
+		self.phase = 3
 
 	def setup(self):
 		self.turt.penup()
 		self.turt.hideturtle()
-		self.turt.width = 10
-		x_0 = self._calc_x(self.x)
-		y_0 = self._calc_y(self.y)
+		self.turt.width(3)
+		x_0 = self.x * self._calc_x(self.x)
+		y_0 = self._calc_y(self.x)
+		print(x_0, y_0)
 		self.turt.goto(x_0, y_0)
 		self.turt.pendown()
 
 	def _calc_x(self, angle):
-		return self.amp * math.cos((angle/10 * self.freq) + self.phase)
-
+		return self.amp * math.cos((angle * self.freq) + self.phase)
 
 	def _calc_y(self, angle):
-		return self.amp * math.sin((angle/10 * self.freq) + self.phase)
+		return self.amp * math.sin((angle * self.freq) + self.phase)
 
 	def draw(self):
-		for t in range(90, -90, -1):
-			self.new_heading = t + self.x
-			self.turt.setheading(self.new_heading)
-			self.turt.forward(1)
+		self.turt.setheading(self.new_heading)
+		self.turt.forward(10)
+
+		# for t in range(self.new_heading, -1 * self.new_heading, -1):
+		# 	self.new_heading += t
+		# 	self.turt.setheading(self.new_heading)
+		# 	self.turt.forward(10)
 
 def run():
 	for a in range(0, 360, 10):
-		arc2 = Arc(a, a*10)
+		arc2 = Arc(a)
 		arc2.setup()
 		arc2.draw()
 
