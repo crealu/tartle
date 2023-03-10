857cd4035d68e9eb3bb36ad044dd6838b4d8946b
crealu <cameron.miller294@gmail.com>
2023-03-10 00:16:59 -0500

    Update README

--- tartle12/tart.py
+++ tartle12/tart.py
@@ -13,7 +13,7 @@ bounds = (366, 140, 1060, 800)
 def run():
 	def trail(t):
 		for i in range(0, 6):
-			random_heading = random.randint(-90, 90)
+			random_heading = random.randint(-45, 45)
 			t.setheading(random_heading)
 			t.forward(10)
 
@@ -23,20 +23,21 @@ def run():
 		turtles.append(t)
 
 	for n in range (0, 20):
+		turtles[n].speed(10)
 		turtles[n].hideturtle()
 		turtles[n].goto(-300, (n*32) - 300)
 		turtles[n].pendown()
-		turtles[n].width(5)
+		turtles[n].width(abs((21 - n)/2))
 		turtles[n].setheading(0)
 		turtles[n].forward(300)
 
 		trail(turtles[n])
 
 	for o in range(0, 20):
-		turtles[o].width(10)
-		turtles[o].goto(360, (o*32) - 310)
+		turtles[o].width(o + 1)
+		turtles[o].goto(60, (o*32) - 310)
 		turtles[o].setheading(0)
-		turtles[o].forward(140)
+		turtles[o].forward(240)
 
 	img = ImageGrab.grab(bbox=bounds)
 	img.save(this_dir + '/tartle12/tart.png', quality=95)
